b5503ee8a15d5ea5d2340c1d3d1488f7a6c70dcc
ORI-Muchim <whals467746@gmail.com>
2023-08-17 02:44:29 +0900

    Edit get_model.py

--- get_model.py
+++ get_model.py
@@ -4,12 +4,14 @@ import requests
 discriminator = './RVC/pretrained_v2/f0D48k.pth'
 generator = './RVC/pretrained_v2/f0G48k.pth'
 hubert = './RVC/hubert_base.pt'
+nuwave2 = './nuwave2/nuwave2.ckpt'
 
 def get_model():
-    if not os.path.isfile(discriminator) or os.path.isfile(generator) or os.path.isfile(hubert):
+    if not os.path.isfile(discriminator) or os.path.isfile(generator) or os.path.isfile(hubert) or os.path.isfile(nuwave2):
         url1 = 'https://github.com/ORI-Muchim/BARK-RVC/releases/download/v1.0/f0D48k.pth'
         url2 = 'https://github.com/ORI-Muchim/BARK-RVC/releases/download/v1.0/f0G48k.pth'
         url3 = 'https://github.com/ORI-Muchim/BARK-RVC/releases/download/v1.0/hubert_base.pt'
+        url4 = 'https://github.com/ORI-Muchim/BARK-RVC/releases/download/v1.0/nuwave2.ckpt'
 
         print("Downloading Pretrained Discriminator Model...")
         response1 = requests.get(url1, allow_redirects=True)
@@ -19,14 +21,19 @@ def get_model():
         
         print("Downloading Hubert Model...")
         response3 = requests.get(url3, allow_redirects=True)
+        
+        print("Downloading Nu-Wave2 Model...")
+        response4 = requests.get(url4, allow_redirects=True)
 
         directory = './RVC/pretrained_v2'
         directory2 = './RVC'
+        directory3 = './nuwave2'
 
         pretrained_discriminator_model = os.path.join(directory, 'f0D48k.pth')
         pretrained_generator_model = os.path.join(directory, 'f0G48k.pth')
         hubert_model = os.path.join(directory2, 'hubert_base.pt')
-
+        nuwave2_model = os.path.join(directory3, 'nuwave2.ckpt')
+        
         with open(pretrained_discriminator_model, 'wb') as file:
             file.write(response1.content)
         print("Saving Pretrained Discriminator Model...")
@@ -37,6 +44,10 @@ def get_model():
         
         with open(hubert_model, 'wb') as file:
             file.write(response3.content)
-        print("Saving Hubert Model...\n")
+        print("Saving Hubert Model...")
+        
+        with open(nuwave2_model, 'wb') as file:
+            file.write(response4.content)
+        print("Saving NU-Wave2 Model...\n")
     else:
         print('Skipping Download... Model exists.')
